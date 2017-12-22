06b42c06d381df46cd689aded76c740f90eea887
ekaterina.bakaldina <ekaterina.bakaldina@roscomputing.com>
2017-12-22 16:25:47 +0300

    Edit1

--- scripts.js
+++ scripts.js
@@ -321,6 +321,26 @@ var chart1 = AmCharts.makeChart("chartdiv1", {
     enabled: true
   }
 });
+var $button1 = document.querySelector(".button-1"),
+  $container1 = document.querySelector(".container-1"),
+  $button2 = document.querySelector(".button-2"),
+  $container2 = document.querySelector(".container-2");
+
+$button1.addEventListener("click", function() {
+  $container1.classList.add("active");
+  $container2.classList.remove("active");
+
+  $button1.classList.add("active");
+  $button2.classList.remove("active");
+});
+
+$button2.addEventListener("click", function() {
+  $container1.classList.remove("active");
+  $container2.classList.add("active");
+
+  $button1.classList.remove("active");
+  $button2.classList.add("active");
+});
 $(document).ready(function() {
   $("#userInfo").on("click", function() {
     $(this)
